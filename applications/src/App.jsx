ac0a17eed60fee9a403d45957dd0572a50bdb6b6
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-12 22:25:00 +0300

    verification email

--- applications/src/App.jsx
+++ applications/src/App.jsx
@@ -7,6 +7,8 @@ import Subdomain from "./views/Subdomain";
 import XssHunter from "./views/XssHunter";
 import SqliHunter from "./views/SqliHunter.";
 import ProfilePage from "./views/ProfilePage";
+import HomePage from "./views/HomePage";
+import VerificationMessage from "./views/VerificationMessage";
 // import PortScanner from "./views/PortScanner";
 // import XssHunter from "./views/XssHunter";
 // import SqliHunter from "./views/SqliHunter.";
@@ -14,7 +16,7 @@ import ProfilePage from "./views/ProfilePage";
 // import XssHunter from "./views/XssHunter";
 
 function App() {
-  return <ProfilePage />;
+  return <VerificationMessage />;
 }
 
 export default App;
