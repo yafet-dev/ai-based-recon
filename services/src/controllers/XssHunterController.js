9126add5f597d136ce3551d27ad2465d1d49d360
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-13 05:44:39 +0300

    working harddd

--- services/src/controllers/XssHunterController.js
+++ services/src/controllers/XssHunterController.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import axios from 'axios';
 
 const xssPayloads = [
   '<script>alert("XSS1")</script>',
@@ -11,47 +11,54 @@ export const testXss = async (req, res) => {
   const { url } = req.query;
 
   if (!url) {
+    console.log('URL is required');
     return res.status(400).send('URL is required');
   }
 
   if (!url.includes('INJECT_HERE')) {
+    console.log('URL must contain the placeholder INJECT_HERE');
     return res.status(400).send('URL must contain the placeholder INJECT_HERE');
   }
 
   const results = [];
 
   try {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-
     for (let payload of xssPayloads) {
-      const encodedPayload = encodeURIComponent(payload);
-      const testUrl = url.replace('INJECT_HERE', encodedPayload);
+      const testUrl = url.replace('INJECT_HERE', encodeURIComponent(payload));
 
       console.log(`Testing URL: ${testUrl}`);
 
-      await page.goto(testUrl, { waitUntil: 'domcontentloaded' });
+      try {
+        const response = await axios.get(testUrl);
 
-      const pageContent = await page.content();
+        const pageContent = response.data;
+        console.log(
+          `Page content length for ${testUrl}: ${pageContent.length}`,
+        );
 
-      if (pageContent.includes(payload)) {
+        if (pageContent.includes(payload)) {
+          results.push({
+            url: testUrl,
+            payload: payload,
+            status: 'vulnerable',
+          });
+        } else {
+          results.push({
+            url: testUrl,
+            payload: payload,
+            status: 'not vulnerable',
+          });
+        }
+      } catch (error) {
+        console.error(`Error navigating to URL: ${testUrl}`, error);
         results.push({
           url: testUrl,
           payload: payload,
-          status: 'vulnerable',
-        });
-      } else {
-        results.push({
-          url: testUrl,
-          payload: payload,
-          status: 'not vulnerable',
+          status: 'error',
         });
       }
-
-      await page.close();
     }
 
-    await browser.close();
     console.log(results); // Log the results to the console
     res.status(200).json(results);
   } catch (error) {
